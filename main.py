3aaf2848e3473f44411be5f3a924b1ba65d1ace0
Ron Wu <rcw278@nyu.edu>
2018-07-15 00:19:57 -0400

    aa

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, session, render_template, jsonify, request
+from flask import Flask, session, render_template, jsonify
 app = Flask(__name__)
 app.secret_key = "super secret key"
 
